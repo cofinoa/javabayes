e83170605cd74384eb9094265c5fe607d8626bc1
cofinoa <cofinoa@gmail.com>
2015-07-07 14:14:29 +0200

    * The scrollbar depends on the node position ranges * Improvement on the drawing algorithm. Only arcs and nodes visible are drawn * Some NulPointerExcception on scrollbar event handler fixed.

--- src/javabayes/Interface/ScrollingPanel.java
+++ src/javabayes/Interface/ScrollingPanel.java
@@ -26,58 +26,77 @@
 
 package javabayes.Interface;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Event;
-import java.awt.Panel;
-import java.awt.Scrollbar;
+import java.awt.*;
 
 public class ScrollingPanel extends Panel {
-	Scrollbar horizontal, vertical;
-	public NetworkPanel netPanel;
+    Scrollbar horizontal, vertical;
+    public NetworkPanel netPanel;
+    
+    static final int VIRTUAL_WIDTH = 100000;
+    static final int VIRTUAL_HEIGHT = 100000;
 
-	static final int VIRTUAL_WIDTH = 10000;
-	static final int VIRTUAL_HEIGHT = 10000;
+    ScrollingPanel(EditorFrame editorFrame) {
+        netPanel = new NetworkPanel(editorFrame, this);
+ 
+        // Start the scrollbars without valid parameters since network is not there.
+        horizontal = new Scrollbar(Scrollbar.HORIZONTAL, 0, VIRTUAL_WIDTH, 0, VIRTUAL_WIDTH);
+        vertical = new Scrollbar(Scrollbar.VERTICAL, 0, VIRTUAL_HEIGHT, 0, VIRTUAL_HEIGHT);
+        
+        setLayout(new BorderLayout());
+        add("Center", netPanel);
+        add("South", horizontal);
+        add("East", vertical);
+        updateScrollbars();
+    }
 
-	ScrollingPanel(EditorFrame editorFrame) {
-		// Start the scrollbars without valid parameters since network is not
-		// there.
-		horizontal = new Scrollbar(Scrollbar.HORIZONTAL, 0, VIRTUAL_WIDTH, 0,
-				VIRTUAL_WIDTH);
-		vertical = new Scrollbar(Scrollbar.VERTICAL, 0, VIRTUAL_HEIGHT, 0,
-				VIRTUAL_HEIGHT);
+    /*
+     * Update the scrollbars.
+     */
+    void updateScrollbars(){
+    	updateScrollbars(false);
+    }
+    void updateScrollbars(boolean upValues) {
+    	Dimension d=netPanel.getSize();
+        int hv,vv;
+    	if(upValues){
+    	    hv=0;
+    	    vv=0;
+    	}else{
+    		hv=horizontal.getValue();
+    		vv=vertical.getValue();
+        }
+        
+    	horizontal.setValues(hv, d.width, 
+    			0, netPanel.x_max);
+    	vertical.setValues(vv, d.height, 0,
+    	                   netPanel.y_max);
 
-		netPanel = new NetworkPanel(editorFrame, this);
+    	horizontal.setBlockIncrement(d.width/2);
+        vertical.setBlockIncrement(d.height/2);
+        horizontal.setUnitIncrement(1);
+        vertical.setUnitIncrement(1);
+    	if(upValues){
+    		netPanel.x_scroll=hv;
+    		netPanel.y_scroll=vv;
+    		netPanel.repaint();
+    	}
+    }
 
-		setLayout(new BorderLayout());
-		add("Center", netPanel);
-		add("South", horizontal);
-		add("East", vertical);
-	}
-
-	/*
-	 * Update the scrollbars.
-	 */
-	void setScrollbars(Dimension d) {
-		horizontal.setValues(horizontal.getValue(), d.width,
-				horizontal.getMinimum(), horizontal.getMaximum());
-		vertical.setValues(vertical.getValue(), d.height,
-				vertical.getMinimum(), vertical.getMaximum());
-	}
-
-	/**
-	 * Handle the scrollbar events (set variables in the NetworkPanel).
-	 */
-	@Override
-	public boolean handleEvent(Event evt) {
-		if (evt.target instanceof Scrollbar) {
-			if (evt.target == horizontal) {
-				netPanel.x_scroll = ((Integer) (evt.arg)).intValue();
-			} else if (evt.target == vertical) {
-				netPanel.y_scroll = ((Integer) (evt.arg)).intValue();
-			}
-			netPanel.repaint();
-		}
-		return (super.handleEvent(evt));
-	}
+    /**
+     * Handle the scrollbar events (set variables in the NetworkPanel).
+     */
+    @Override
+    public boolean handleEvent(Event evt) {
+        Integer i;
+    	if (evt.target instanceof Scrollbar && evt.arg != null) {
+    		i=(Integer)(evt.arg);
+    		if (evt.target == horizontal) {
+            	 netPanel.x_scroll = i.intValue();
+            } else if (evt.target == vertical) {
+            	netPanel.y_scroll = i.intValue();
+            }
+            netPanel.repaint();
+        }
+        return(super.handleEvent(evt));
+    } 
 }
